409bb624355f5a7c6e3cd52777aa2cc542db96ac
hadeer <hadeeressam21@gmail.com>
2025-04-04 20:13:10 +0200

    add the lind of documentation in config file

--- config.js
+++ config.js
@@ -3,3 +3,6 @@ import Config from "react-native-config";
 export default {
     apiIrl: Config.API_URL
 }
+
+// the document CI/CD for React Natuve in Medium is =>
+// https://medium.com/@tusharkumar27864/best-practices-for-ci-cd-in-react-native-projects-cc2340414715
